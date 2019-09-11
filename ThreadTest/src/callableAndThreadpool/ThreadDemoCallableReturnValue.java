e76fef3f0ac7b963f41ee52c2487dd1b573c5dee
GoTime666 <2811660875@qq.com>
2019-09-11 22:53:18 +0800

    去除引用包

--- ThreadTest/src/callableAndThreadpool/ThreadDemoCallableReturnValue.java
+++ ThreadTest/src/callableAndThreadpool/ThreadDemoCallableReturnValue.java
@@ -1,6 +1,5 @@
 package callableAndThreadpool;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
