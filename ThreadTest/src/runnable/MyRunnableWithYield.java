a470ba877a986b496d17875600280446710a1931
GoTime666 <2811660875@qq.com>
2019-09-12 22:50:31 +0800

    MyRunnableWithYield做完

--- ThreadTest/src/runnable/MyRunnableWithYield.java
+++ ThreadTest/src/runnable/MyRunnableWithYield.java
@@ -1,11 +1,18 @@
 package runnable;
 
 public class MyRunnableWithYield implements Runnable {
-
+	
+	public MyRunnableWithYield() {
+		System.out.println("任务启动");
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
+		for (int i = 0; i < 3; i++) {
+			System.out.println("run");
+			Thread.yield();
+		}
 	}
 
 }
