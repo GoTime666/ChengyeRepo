f154ec1ffa4b4a3be7b8cd5405cc87fca92d4691
GoTime666 <2811660875@qq.com>
2019-09-12 15:04:17 +0800

    死循环中break和return return没有结束循环

--- ThreadTest/src/threadSafe/ThreadSafeDemo.java
+++ ThreadTest/src/threadSafe/ThreadSafeDemo.java
@@ -2,20 +2,25 @@ package threadSafe;
 
 public class ThreadSafeDemo {
 	public static void main(String[] args) {
-		//线程不安全
-		//Ticket ticket = new TicketNotSafe();
-		
-		//线程安全
-		//Ticket ticket = new TicketSafeSynchronizedBlock();
-		
-		//线程安全
+		// 线程不安全
+		// Ticket ticket = new TicketNotSafe();
+
+		// 线程安全
+		// Ticket ticket = new TicketSafeSynchronizedBlock();
+
+		// 线程安全
 		Ticket ticket = new TicketSafeSynchronizedFunction();
-		
+
 		Thread t1 = new Thread(ticket, "窗口1");
 		Thread t2 = new Thread(ticket, "窗口2");
 		Thread t3 = new Thread(ticket, "窗口3");
 		t1.start();
 		t2.start();
 		t3.start();
+	}
+	
+	public static void fun() {
+		System.out.println("11");
+		return;
 	}
 }
