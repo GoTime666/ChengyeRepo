fa58f2a309161f28c205b123c747d1dc62934bea
GoTime666 <2811660875@qq.com>
2019-09-12 10:56:23 +0800

    奥术大师多

--- ThreadTest/src/threadSafe/TicketSafe.java
+++ ThreadTest/src/threadSafe/TicketSafe.java
@@ -1,12 +1,11 @@
 package threadSafe;
 
 public class TicketSafe extends Ticket {
-	
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		while (ticket <= 100&&ticket>2) {
+		while (ticket <= 100 && ticket > 2) {
 			synchronized (lock) {
 				try {
 					Thread.sleep(10);
@@ -14,7 +13,7 @@ public class TicketSafe extends Ticket {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				System.out.println(Thread.currentThread().getName()+"正在卖票:"+ticket--);
+				System.out.println(Thread.currentThread().getName() + "正在卖票:" + ticket--);
 			}
 		}
 
