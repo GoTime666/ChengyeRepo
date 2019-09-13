d65829da1d644fd1e4a0e492661d2bd9827fc1be
GoTime666 <2811660875@qq.com>
2019-09-13 11:32:19 +0800

    后台进程daemon

--- ThreadTest/src/thread/ThreadDemo.java
+++ ThreadTest/src/thread/ThreadDemo.java
@@ -1,9 +1,10 @@
 package thread;
 
 public class ThreadDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		MyThread1 t1=new MyThread1("我的线程1");
 		t1.start();
+		//Thread.sleep(2);
 		for(int i=1;i<100;i++)
 			System.out.println("主线程:   "+i);
 		Thread.yield();
