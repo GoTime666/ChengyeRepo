0b55aa8fb2bffc07736acfc0fe2257d383a17de4
GoTime666 <2811660875@qq.com>
2019-09-13 15:49:22 +0800

    1111

--- ReflectTest/src/bean/Person.java
+++ ReflectTest/src/bean/Person.java
@@ -5,11 +5,11 @@ public class Person {
 	public int age;
 	private String address;
 
-	public Person(){
+	public Person() {
 		System.out.println("空参数构造方法");
 	}
 
-	public Person(String name)  throws Exception{
+	public Person(String name) throws Exception {
 		this.name = name;
 		System.out.println("带有String的构造方法");
 	}
@@ -20,6 +20,10 @@ public class Person {
 		System.out.println("带有String ,int的构造方法");
 	}
 
+	public static Person getInstance(String name, int age) {
+		return new Person(name, age);
+	}
+
 	public Person(String name, int age, String address) {
 		super();
 		this.name = name;
@@ -27,25 +31,25 @@ public class Person {
 		this.address = address;
 		System.out.println("带有String,int,String的构造方法");
 	}
-	
+
 	public void method1() {
 		System.out.println("没有返回值没有参数的方法");
 	}
-	
+
 	public void method2(String name) {
-		System.out.println("没有返回值,有参数的方法   name= "+name);
+		System.out.println("没有返回值,有参数的方法   name= " + name);
 	}
-	
+
 	public int method3() {
 		System.out.println("有返回值,没有参数的方法");
 		return 123;
 	}
-	
+
 	public String method4(String name) {
 		System.out.println("有返回值");
-		return "哈哈"+name;
+		return "哈哈" + name;
 	}
-	
+
 	private void method5() {
 		System.out.println("私有方法");
 	}
@@ -54,6 +58,5 @@ public class Person {
 	public String toString() {
 		return "Person [name=" + name + ", age=" + age + ", address=" + address + "]";
 	}
-	
-	
+
 }
--- ReflectTest/src/reflect/ThreeWaysOfGetClass.java
+++ ReflectTest/src/reflect/ThreeWaysOfGetClass.java
@@ -1,50 +1,16 @@
 package reflect;
 
-import java.lang.reflect.Constructor;
-
-import org.junit.jupiter.api.Test;
-
 import bean.Person;
 
-public class ReflectDemo {
+public class ThreeWaysOfGetClass {
 	public static void main(String[] args) throws ClassNotFoundException {
-		Person p=new Person();
+		Person p = new Person();
 		Class<? extends Person> class1 = p.getClass();
-		Class<Person> c=Person.class;
+		Class<Person> c = Person.class;
 		Class<?> class2 = Class.forName("bean.Person");
-		System.out.println("p.getClass() "+class1);
-		System.out.println("Person.class "+c);
-		System.out.println("Class.forName() "+class2);
+		System.out.println("p.getClass() " + class1);
+		System.out.println("Person.class " + c);
+		System.out.println("Class.forName() " + class2);
 	}
-	
-	@Test
-	public void fun1() throws ClassNotFoundException, NoSuchMethodException, SecurityException {
-		Class<?> class1 = Class.forName("bean.Person");
-		Constructor<?>[] constructors = class1.getConstructors();
-		for (Constructor<?> constructor : constructors) {
-			System.out.println(constructor);
-		}
-		System.out.println();
-		
-		Constructor<?>[] declaredConstructors = class1.getDeclaredConstructors();
-		for (Constructor<?> constructor : declaredConstructors) {
-			System.out.println(constructor);
-		}
-		System.out.println();
-		
-		Constructor<?> constructor = class1.getConstructor(null);
-		System.out.println(constructor);
-		
-		Constructor<?> constructor2 = class1.getConstructor(String.class);
-		System.out.println(constructor2);
-		
-		Constructor<?> declaredConstructor = class1.getDeclaredConstructor(String.class,int.class);
-		System.out.println(declaredConstructor);
-		
-		Constructor<?> declaredConstructor2 = class1.getDeclaredConstructor(String.class,int.class,String.class);
-		System.out.println(declaredConstructor2);
-		
-		
-		
-	}
+
 }
