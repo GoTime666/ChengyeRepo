edf13f6d10e904f734b442c38c963d3ba0b4b624
GoTime666 <2811660875@qq.com>
2019-09-13 11:32:54 +0800

    Revert "后台进程daemon"
    This reverts commit d65829da1d644fd1e4a0e492661d2bd9827fc1be.

--- ThreadTest/src/thread/ThreadDemo.java
+++ ThreadTest/src/thread/ThreadDemo.java
@@ -1,10 +1,9 @@
 package thread;
 
 public class ThreadDemo {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		MyThread1 t1=new MyThread1("我的线程1");
 		t1.start();
-		//Thread.sleep(2);
 		for(int i=1;i<100;i++)
 			System.out.println("主线程:   "+i);
 		Thread.yield();
