22fb88fc7f6d153552e503f2d55699ea9c302eb7
GoTime666 <2811660875@qq.com>
2019-09-14 14:22:16 +0800

    通过反射调用方法

--- ReflectTest/src/reflect/GetMethodAndUse.java
+++ ReflectTest/src/reflect/GetMethodAndUse.java
@@ -1,33 +1,37 @@
 package reflect;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class GetMethodAndUse {
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+			IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
 		Class<?> class1 = Class.forName("bean.Person");
-		//Method[] methods = class1.getMethods();
+		// Method[] methods = class1.getMethods();
 		for (Method method : class1.getMethods()) {
 			System.out.println(method);
 		}
 		System.out.println();
-		
+
 		for (Method method : class1.getDeclaredMethods()) {
 			System.out.println(method);
 		}
 		System.out.println();
-		
+
 		Method method1 = class1.getMethod("method1");
 		System.out.println(method1);
-		
+
 		Method method4 = class1.getMethod("method4", String.class);
 		System.out.println(method4);
-		
+
 		Method method5 = class1.getDeclaredMethod("method5");
+		method5.setAccessible(true);
 		System.out.println(method5);
-		
-		
-		
-		
-		
+
+		Constructor<?> constructor = class1.getConstructor();
+		Object obj = constructor.newInstance();
+		Object invoke = method5.invoke(obj);
+		System.out.println(invoke);
 	}
 }
